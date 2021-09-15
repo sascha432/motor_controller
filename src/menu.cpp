7bec1ac4f94c2ffe83b7549935cd8073caefa39e
sascha <sascha_lammers@gmx.de>
2021-09-15 12:01:07 -0700

    replaced reference with global, pass menu as a single string

--- src/menu.cpp
+++ src/menu.cpp
@@ -4,108 +4,20 @@
 
 #include "main.h"
 
-Menu::Menu(uint8_t count, Adafruit_SSD1306 &display) : _position(0), _size(count), _state(0), _display(display)
-{
-    _items = new PGM_P[count];
-}
-
-Menu::~Menu()
-{
-    delete _items;
-}
-
 void Menu::display()
 {
-    _display.clearDisplay();
-    _display.setTextSize(1);
-    _displayItem(0, _position + _size - 1);
+    __display().clearDisplay();
+    __display().setTextSize(1);
+    _displayItem(0, _position + size() - 1);
     _displayItem(12, _position);
     _displayItem(24, _position + 1);
-    // highlighjt
-    _display.fillRoundRect(5, 10, SCREEN_WIDTH - 15, 12, 4, SSD1306_INVERTDISPLAY);
+    // highlight
+    __display().fillRoundRect(5, 10, SCREEN_WIDTH - 15, 12, 4, SSD1306_INVERSE);
 
     // scrollbar
-    _display.drawRoundRect(SCREEN_WIDTH - 8, 0, 8, SCREEN_HEIGHT, 3, WHITE);
-    uint8_t y = 2 + (_position * (SCREEN_HEIGHT - 6) / _size);
-    _display.fillRoundRect(SCREEN_WIDTH - 7, y, 6, 6, 3, WHITE);
+    __display().drawRoundRect(SCREEN_WIDTH - 8, 0, 8, SCREEN_HEIGHT, 3, WHITE);
+    uint8_t y = 2 + (_position * (SCREEN_HEIGHT - 6) / size());
+    __display().fillRoundRect(SCREEN_WIDTH - 7, y, 6, 6, 3, WHITE);
 
-    _display.display();
-}
-
-void Menu::displayTitle()
-{
-    _display.setTextSize(1);
-    _displayItem(0, _position);
-}
-
-void Menu::add(uint8_t index, PGM_P name)
-{
-    _items[index] = name;
-}
-
-bool Menu::isOpen() const
-{
-    return _state != 0;
-}
-
-bool Menu::isActive() const
-{
-    return _state == 2;
-}
-
-bool Menu::isMainMenuActive() const
-{
-    return _state == 1;
-}
-
-void Menu::close()
-{
-    _state = 0;
-}
-
-void Menu::open()
-{
-    _state = 1;
-    display();
-}
-
-void Menu::up()
-{
-    _position = (_position - 1) % _size;
-    display();
-}
-
-void Menu::down()
-{
-    _position = (_position + 1) % _size;
-    display();
-}
-
-bool Menu::setPosition(uint8_t position)
-{
-    position = position % _size;
-    if (_position != position) {
-        _position = position;
-        display();
-        return true;
-    }
-    return false;
-}
-
-void Menu::enter()
-{
-    _state = 2;
-}
-
-void Menu::exit()
-{
-    _state = 1;
-    display();
-}
-
-void Menu::_displayItem(uint8_t y, uint8_t index)
-{
-    auto item = _items[index % _size];
-    _display.setCursor((SCREEN_WIDTH / 2) - (strlen_P(item) * (FONT_WIDTH / 2)), y);
-    _display.print(FPSTR(item));
+    __display().display();
 }
--- src/menu.h
+++ src/menu.h
@@ -21,32 +21,30 @@ enum class MenuEnum : uint8_t {
     MENU_PWM,
     MENU_STALL,
     MENU_BRAKE,
+    MENU_MOTOR,
     MENU_INFO,
+    MENU_RESTORE,
     MENU_EXIT,
-    MENU_COUNT,
+    MENU_COUNT
 };
 
 class Menu {
 public:
-    Menu(uint8_t count, Adafruit_SSD1306 &display);
-    inline Menu(MenuEnum count, Adafruit_SSD1306 &display) :
-        Menu(static_cast<uint8_t>(count), display) { }
-    ~Menu();
+    static constexpr size_t kMenuSize =  static_cast<size_t>(MenuEnum::MENU_COUNT);
+
+public:
+    // Menu(Adafruit_SSD1306 &display);
+    Menu();
 
     void display();
     void displayTitle();
 
+    void add(PGM_P items);
     void add(uint8_t index, PGM_P name);
-    inline void add(MenuEnum index, PGM_P name)
-    {
-        add(static_cast<uint8_t>(index), name);
-    }
+    void add(MenuEnum index, PGM_P name);
 
     bool isOpen() const;
-    bool isClosed() const
-    {
-        return !isOpen();
-    }
+    bool isClosed() const;
     bool isActive() const;
     bool isMainMenuActive() const;
     void open();
@@ -55,23 +53,12 @@ public:
     void up();
     void down();
     bool setPosition(uint8_t position);
-    inline bool setPosition(MenuEnum position)
-    {
-        return setPosition(static_cast<uint8_t>(position));
-    }
+    bool setPosition(MenuEnum position);
+    uint8_t getPositionInt() const;
+    MenuEnum getPosition() const;
 
-    uint8_t getPositionInt() const
-    {
-        return _position;
-    }
-    inline MenuEnum getPosition() const
-    {
-        return static_cast<MenuEnum>(_position);
-    }
-
-    inline uint8_t getSize() const
-    {
-        return _size;
+    constexpr uint8_t size() const {
+        return kMenuSize;
     }
 
     void enter();
@@ -80,10 +67,138 @@ public:
 private:
     void _displayItem(uint8_t y, uint8_t index);
 
+    constexpr Adafruit_SSD1306 &__display() {
+        return ::display;
+    }
+    // Adafruit_SSD1306 &__display() {
+    //     return _display;
+    // }
+
 private:
+    // Adafruit_SSD1306 &_display;
+    PGM_P _items[kMenuSize];
     uint8_t _position;
-    uint8_t _size;
     uint8_t _state;
-    PGM_P *_items;
-    Adafruit_SSD1306 &_display;
 };
+
+inline Menu::Menu() : _position(0), _state(0)
+// inline Menu::Menu(Adafruit_SSD1306 &display) : _display(display), _position(0), _state(0)
+{
+}
+
+inline void Menu::add(PGM_P items)
+{
+    auto ptr = &_items[0];
+    auto startPtr = items;
+    while(pgm_read_byte(startPtr)) {
+        *ptr++ = startPtr;
+        // Serial.println(FPSTR(startPtr));
+        while(pgm_read_byte(startPtr)) {
+            startPtr++;
+        }
+        startPtr++;
+    }
+}
+
+inline void Menu::add(MenuEnum index, PGM_P name)
+{
+    add(static_cast<uint8_t>(index), name);
+}
+
+inline bool Menu::setPosition(MenuEnum position)
+{
+    return setPosition(static_cast<uint8_t>(position));
+}
+
+inline uint8_t Menu::getPositionInt() const
+{
+    return _position;
+}
+
+inline MenuEnum Menu::getPosition() const
+{
+    return static_cast<MenuEnum>(_position);
+}
+
+inline void Menu::displayTitle()
+{
+    __display().setTextSize(1);
+    _displayItem(0, _position);
+}
+
+inline void Menu::add(uint8_t index, PGM_P name)
+{
+    _items[index] = name;
+}
+
+inline bool Menu::isOpen() const
+{
+    return _state != 0;
+}
+
+inline bool Menu::isClosed() const
+{
+    return !isOpen();
+}
+
+inline bool Menu::isActive() const
+{
+    return _state == 2;
+}
+
+inline bool Menu::isMainMenuActive() const
+{
+    return _state == 1;
+}
+
+inline void Menu::close()
+{
+    _state = 0;
+}
+
+inline void Menu::open()
+{
+    _state = 1;
+    display();
+}
+
+inline void Menu::up()
+{
+    _position = (_position - 1) % size();
+    display();
+}
+
+inline void Menu::down()
+{
+    _position = (_position + 1) % size();
+    display();
+}
+
+inline bool Menu::setPosition(uint8_t position)
+{
+    position = position % size();
+    if (_position != position) {
+        _position = position;
+        display();
+        return true;
+    }
+    return false;
+}
+
+inline void Menu::enter()
+{
+    _state = 2;
+}
+
+inline void Menu::exit()
+{
+    _state = 1;
+    display();
+}
+
+inline void Menu::_displayItem(uint8_t y, uint8_t index)
+{
+    auto item = _items[index % size()];
+    __display().setCursor((SCREEN_WIDTH / 2) - (strlen_P(item) * (FONT_WIDTH / 2)), y);
+    __display().print(FPSTR(item));
+}
